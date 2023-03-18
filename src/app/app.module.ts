823b326d10877c8604e9f3ce4bc37e225169fec6
MatheusPereira00 <matheuspereira@ibm.com>
2023-03-18 16:29:25 -0300

    add product module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +7,8 @@ import { FooterComponent } from './components/layout/footer/footer.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { SidenavComponent } from './components/layout/sidenav/sidenav.component';
 
-
-
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, SidenavComponent,],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, SidenavComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
