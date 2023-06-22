94299da2e52b03bf3cb07c8f4367f9ec7cc564d0
MatheusPereira00 <matheuspereira@ibm.com>
2023-06-22 10:53:40 -0300

    att

---
+++ src/app/components/services/local-storage.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  // storage: Storage;
+
+  constructor() { }
+  
+  // getDarkModeStatus(){
+  //   let value: 
+  // }
+
+
+
+}
